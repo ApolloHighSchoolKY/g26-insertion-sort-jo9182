fd96ec4fd4ea47385c4f581e5a84dc4f00f19a18
jo9182 <73309930+jo9182@users.noreply.github.com>
2025-04-21 04:23:33 +0000

    I think this works? How does this work???

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,9 +1,28 @@
+
 public class AInsertionSort
 {
 
     public static void main(String[] args)
     {
-    	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
+        Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
 
+
+        for (int i = 1; i < myNumbers.length; i++) {
+            int guide = myNumbers[i];
+
+            // Wowza, didn't know you could do this. Very useful.
+            int j;
+
+            for (j = i - 1; j >= 0 && myNumbers[j] > guide; j--) {
+                myNumbers[j + 1] = myNumbers[j];
+            }
+
+            //Because for some reason my code is allergic to i
+            myNumbers[j + 1] = guide;
+        }
+
+        //Was there a different way to do this? 
+        for(int num : myNumbers)
+            System.out.println(num);
+        }
     }
-}
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -5,31 +5,36 @@ public class ALInsertionSort
 
     public static void main(String[] args)
     {
-    	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
-    	ArrayList<Integer> sorted = new ArrayList<Integer>();
-    	boolean inserted=false;
+        Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
+        ArrayList<Integer> sorted = new ArrayList<Integer>();
+        boolean inserted = false;
 
-		//Add the first number to the array list
+        // Add the first number to the array list
+        sorted.add(myNumbers[0]);
+        
+        // Loop once for all of the remaining numbers in the unsorted list
+        for (int i = 1; i < myNumbers.length; i++) {
+            // Check with each of the numbers in the sorted list
+            for (int j = 0; j < sorted.size(); j++) {
+                // If this number is less than one in the sorted list,
+                // insert it there
+                if (myNumbers[i] < sorted.get(j) && inserted == false) {
+                    sorted.add(j, myNumbers[i]);
+                    inserted = true;
+                }
+            }
+            
+            // If it was not inserted, stick it on the end
+            if (inserted == false) {
+                sorted.add(myNumbers[i]);
+            }
+            inserted = false; 
+        }
 
-		//Loop once for all of the remaining numbers in the unsorted list
+        // Print the sorted list
+        System.out.println(sorted);
 
-			//Check with each of the numbers in the sorted list
-
-				//If this number is less than one in the sorted list,
-				//insert it there
-
-			//If it was not inserted, stick it on the end.
-
-
-		//End Loop for unsorted list
-
-		System.out.println(sorted);
-
-		//Move the data back to the array
-
-		//Print the contents of the array
-
+        // Move the data back to the array (if needed)
+        // Print the contents of the array (if needed)
     }
-
-
 }
